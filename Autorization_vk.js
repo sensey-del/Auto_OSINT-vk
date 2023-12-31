6002c3d7c80ebdfc2f82c04e9dce709b67245a73
S3NSEY <67517450+sensey-del@users.noreply.github.com>
2023-12-31 15:45:12 +0300

    Update Autorization_vk.js

--- Autorization_vk.js
+++ Autorization_vk.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
   try {
     await page.goto('https://vk.com/login');
     await page.waitForSelector('#index_email');
-    await page.type('#index_email', '89117243799'); // Замените 'ВАШ_ЛОГИН' на реальный логин
+    await page.type('#index_email', '<login>'); // Замените 'ВАШ_ЛОГИН' на реальный логин
     await page.keyboard.press('Enter')
     await page.waitForTimeout(15000)
     await page.waitForNavigation();
